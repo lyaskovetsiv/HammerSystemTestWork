a2bc72227573bfad069eaa3af91d3373d7b8b182
Ivan Lyaskovets <lyaskovets.iv@gmail.com>
2023-06-22 12:39:45 +0300

    Initial commit

---
+++ HammerSystemTestWork/MainModule/View/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  HammerSystemTestWork
+//
+//  Created by Ivan Lyaskovets on 22.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+		view.backgroundColor = .red
+	}
+}
+
